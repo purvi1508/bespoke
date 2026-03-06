purvi1508/bespoke

commit 1a471e00459df7e6797ebd946b790ac58eae162e

Adds a flag to set starting difficulty
Using this flag, vocabulary of the given difficulty or below will not
be introduced. If you do fail it though, it will be learned as usual.

To make the new feature more testable, this commit adds dependency
injection for CardIndex. Also adds simple tests.

bespoke/deck.py
@@ -82,14 +82,17 @@ class Deck:
         self,
         target_language: Language,
         native_language: Language,
+        card_index: CardIndex,
     ) -> None:
         self._target_language = target_language
         self._native_language = native_language
-        self._card_index = CardIndex.load(target_language, native_language)
+        self._card_index = card_index
         self._ratings: dict[str, list[Rating]] = {}
         self._card_id_uses: dict[str, list[CardUsage]] = {}
         self._difficulty = Difficulty.A1
         self._modes = list(Mode)
+        self._assume_known = None
+        self._start_index = 0
         self._full_vocabulary = target_language.full_vocabulary()
         self._difficulty_map = {}
         for difficulty in Difficulty:
@@ -104,16 +107,18 @@ class Deck:
             if (touched + TOUCH_MARGIN) / (i + TOUCH_MARGIN) < MINIMUM_TOUCH_RATIO:
                 has_reached_threshold = True
             history = self._ratings.get(unit)
+            is_touched = i < self._start_index
+            if is_touched:
+                touched += 1
             if history is None or _is_untouched(history):
                 urgency_states[unit] = UrgencyState(
-                    is_touched=False,
+                    is_touched=is_touched,
                     needs_introduction=False,
                     is_target=not has_reached_threshold,
                     urgency=0.0,
                     mode=self._modes[0],
                 )
                 continue
-            touched += 1
             highest_urgency, mode = max(
                 (compute_urgency(history, m, current_time), m) for m in self._modes
             )
@@ -236,6 +241,19 @@ class Deck:
     def set_modes(self, modes: list[Mode]) -> None:
         self._modes = modes
 
+    def set_assume_known(self, difficulty: Difficulty | None) -> None:
+        self._assume_known = difficulty
+        self._start_index = 0
+        if difficulty is not None:
+            for d in Difficulty:
+                self._start_index += len(self._target_language.vocabulary(d))
+                if d == difficulty:
+                    break
+        if self._start_index >= len(self._full_vocabulary):
+            print("Cannot set minimum difficulty.")
+            self._assume_known = None
+            self._start_index = 0
+
     def log_feedback(self, modes: list[Mode]) -> None:
         self._modes = modes
 
@@ -269,6 +287,8 @@ class Deck:
             "difficulty": str(self._difficulty),
             "modes": [str(m) for m in self._modes],
         }
+        if self._assume_known is not None:
+            data["assume_known"] = str(self._assume_known)
         with open(filename, "w", encoding="utf-8") as f:
             json.dump(data, f)
 
@@ -276,10 +296,10 @@ class Deck:
     def load(cls, filename: Path | str) -> Self:
         with open(filename, "r", encoding="utf-8") as f:
             data = json.load(f)
-        deck = cls(
-            LANGUAGES[data["target_language"]],
-            LANGUAGES[data["native_language"]],
-        )
+        target_language = LANGUAGES[data["target_language"]]
+        native_language = LANGUAGES[data["native_language"]]
+        card_index = CardIndex.load(target_language, native_language)
+        deck = cls(target_language, native_language, card_index)
         for key, ratings_data in data["ratings"].items():
             ratings = list(Rating.model_validate(r) for r in ratings_data)
             deck._ratings[key] = ratings
@@ -288,4 +308,7 @@ class Deck:
             deck._card_id_uses[key] = usages
         deck._difficulty = Difficulty(data["difficulty"])
         deck._modes = [Mode(m) for m in data["modes"]]
+        assume_known = data.get("assume_known")
+        if assume_known is not None:
+            deck.set_assume_known(Difficulty(assume_known))
         return deck

bespoke/builder.py
@@ -205,10 +205,10 @@ class DeckBuilder:
     def __init__(
         self,
         target_language: Language,
-        native_language: Language,
+        card_index: CardIndex,
     ) -> None:
         self._language = target_language
-        self._card_index = CardIndex.load(target_language, native_language)
+        self._card_index = card_index
         self._full_vocabulary = self._language.full_vocabulary()
         self._duplicates = set()
         self._start_time = None

create.py
@@ -28,12 +28,12 @@ async def create(
     cards_per_unit: int,
     cards_per_call: int,
 ) -> None:
-    deck_builder = DeckBuilder(target, native)
+    card_index = CardIndex.load(target, native)
+    deck_builder = DeckBuilder(target, card_index)
     await deck_builder.create_cards(
         cards_per_unit=cards_per_unit,
         cards_per_call=cards_per_call,
     )
-    card_index = CardIndex.load(target, native)
     await card_index.check()
 
 

learn.py
@@ -20,6 +20,7 @@ import os
 from pathlib import Path
 import sys
 
+from bespoke import CardIndex
 from bespoke import Deck
 from bespoke import Difficulty
 from bespoke import Mode
@@ -250,11 +251,17 @@ def open_deck() -> tuple[Deck, str]:
         type=str,
         choices=list(difficulties),
         required=True,
-        help="A language that you know.",
+        help="Your estimated level in that language.",
     )
     parser.add_argument("--use_read_mode", action="store_true", help="Enable read mode")
     parser.add_argument(
         "--use_write_mode", action="store_true", help="Enable write mode"
+    )
+    parser.add_argument(
+        "--assume_known",
+        type=str,
+        choices=list(difficulties),
+        help="Words of this level (inclusive) are assumed known until failed.",
     )
     args = parser.parse_args()
 
@@ -270,12 +277,14 @@ def open_deck() -> tuple[Deck, str]:
     deck_filename = f"deck_{target.code_name}.json"
     if not os.path.isfile(deck_filename):
         print("Creating a new deck...")
-        deck = Deck(target, native)
+        card_index = CardIndex(target, native)
+        deck = Deck(target, native, card_index)
         deck.save(deck_filename)
     else:
         deck = Deck.load(deck_filename)
     deck.set_difficulty(difficulty)
     deck.set_modes(modes)
+    deck.set_assume_known(args.assume_known)
 
     return deck, deck_filename
 

tests/test_deck.py
@@ -1,0 +1,93 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+from bespoke import Card
+from bespoke import Deck
+from bespoke import Difficulty
+from bespoke import Language
+from bespoke import Mode
+from bespoke import languages
+
+
+class TestIndex:
+    def __init__(
+        self,
+        target_language: Language,
+        native_language: Language,
+    ) -> None:
+        del native_language
+        self._target_language = target_language
+
+    def cards(self, unit: str) -> list[Card]:
+        if unit not in self._target_language.full_vocabulary():
+            return []
+        card = Card(
+            id=unit,
+            sentence=unit,
+            native_sentence="dummy",
+            audio_filename="fake.ogg",
+            slow_audio_filename="slow.ogg",
+            native_audio_filename="native.ogg",
+            phonetic="abc",
+            units=[unit],
+            unit_tags={unit: unit},
+            notes=[],
+        )
+        return [card]
+
+    def size(self, unit: str) -> int:
+        return int(unit in self._target_language.full_vocabulary())
+
+
+class TestDeck(unittest.TestCase):
+    def test_draw(self) -> None:
+        target = languages.LANGUAGES["japanese"]
+        native = languages.LANGUAGES["english"]
+        index = TestIndex(target, native)
+        deck = Deck(target, native, index)
+        deck.set_modes([Mode.LISTEN, Mode.SPEAK])
+        mode, card = deck.draw()
+        unit = target.vocabulary(Difficulty.A1)[0]
+        self.assertEqual(mode, Mode.LISTEN)
+        self.assertEqual(card.sentence, unit)
+
+    def test_rate(self) -> None:
+        target = languages.LANGUAGES["japanese"]
+        native = languages.LANGUAGES["english"]
+        index = TestIndex(target, native)
+        deck = Deck(target, native, index)
+        deck.set_modes([Mode.LISTEN, Mode.SPEAK])
+        mode, card = deck.draw()
+        unit = target.vocabulary(Difficulty.A1)[0]
+        self.assertEqual(card.units, [unit])
+        deck.rate(unit, mode, 3)
+        _mode, card = deck.draw()
+        unit = target.vocabulary(Difficulty.A1)[1]
+        self.assertEqual(card.units, [unit])
+
+    def test_assume_known(self) -> None:
+        target = languages.LANGUAGES["japanese"]
+        native = languages.LANGUAGES["english"]
+        index = TestIndex(target, native)
+        deck = Deck(target, native, index)
+        deck.set_assume_known(Difficulty.A2)
+        _mode, card = deck.draw()
+        unit = target.vocabulary(Difficulty.B1)[0]
+        self.assertEqual(card.sentence, unit)
+
+
+if __name__ == "__main__":
+    unittest.main()

commit fbe2ae759f36e43e4af08001470e13852e3031cc

Fixes incorrect touched card count
The fixed bug was introduced in 1a471e0.
Now, units that learned block harder units correctly.

bespoke/deck.py
@@ -107,10 +107,10 @@ class Deck:
             if (touched + TOUCH_MARGIN) / (i + TOUCH_MARGIN) < MINIMUM_TOUCH_RATIO:
                 has_reached_threshold = True
             history = self._ratings.get(unit)
-            is_touched = i < self._start_index
-            if is_touched:
-                touched += 1
             if history is None or _is_untouched(history):
+                is_touched = i < self._start_index
+                if is_touched:
+                    touched += 1
                 urgency_states[unit] = UrgencyState(
                     is_touched=is_touched,
                     needs_introduction=False,
@@ -119,6 +119,7 @@ class Deck:
                     mode=self._modes[0],
                 )
                 continue
+            touched += 1
             highest_urgency, mode = max(
                 (compute_urgency(history, m, current_time), m) for m in self._modes
             )